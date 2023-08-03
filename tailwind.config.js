53243b0290bcfc1cbb47f1f3662a110104726cc1
PAULO CESAR FERREIRA DO AMARANTE <paulo_amarante@estudante.sesisenai.org.br>
2023-08-03 09:43:12 -0300

    a

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./www/atividade-2.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
